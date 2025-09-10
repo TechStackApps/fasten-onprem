11e4220d72dfc5d649c077a5258613c2a83e9b1e
Beatrix | TSA.team <hello@techstackapps.com>
2025-09-10 17:14:42 +0300

    WIP: salvare temporară

--- frontend/e2e/protractor.conf.js
+++ frontend/e2e/protractor.conf.js
@@ -1,32 +1,60 @@
-// @ts-check
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
+const path = require('path');
 
-const { SpecReporter } = require('jasmine-spec-reporter');
-
-/**
- * @type { import("protractor").Config }
- */
 exports.config = {
   allScriptsTimeout: 11000,
+
+
   specs: [
-    './src/**/*.e2e-spec.ts'
+    //'./src/**/*.e2e-spec.ts', 
+    './src/login.e2e-spec.ts',
+    './src/dashboard.e2e-spec.ts',
+    './src/sources.e2e-spec.ts',
+    './src/backGroundJobHistory.e2e-spec.ts',
+    './src/medicalHistory.e2e-spec.ts',
+    './src/users.e2e-spec.ts',
+    './src/labs.e2e-spec.ts'
   ],
+
   capabilities: {
-    browserName: 'chrome'
+    browserName: 'chrome',
+    args: ['--headless=new', '--disable-notifications', '--disable-dev-shm-usage'],
+
+    chromeOptions: {
+      args: [
+        //'--headless',              
+        '--disable-infobars',
+        '--no-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+        '--disable-software-rasterizer',
+        '--log-level=3'
+      ],
+      excludeSwitches: ['enable-logging'],
+
+      prefs: {
+        'download.default_directory': path.resolve(__dirname, 'downloads'),
+        'download.prompt_for_download': false,
+        'download.directory_upgrade': true,
+        'plugins.always_open_pdf_externally': true
+      }
+    }
   },
+
+  chromeDriver: '../chromedriver-win64/chromedriver.exe',
+
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () { }
   },
   onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.json')
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    const { SpecReporter } = require('jasmine-spec-reporter');
+    jasmine.getEnv().addReporter(new SpecReporter());
   }
 };
